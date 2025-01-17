c4f2c13cd3d421842de6a02a3aec0e43f28c8efd
Kevin Zakka <zakka@google.com>
2025-01-17 11:58:36 -0800

    Ensure we check out a specific commit of Menagerie. Fixes #20.
    PiperOrigin-RevId: 716748884
    Change-Id: I39dbae49c48fe2875b30fc5c0063ca7c5da68701

--- mujoco_playground/_src/mjx_env.py
+++ mujoco_playground/_src/mjx_env.py
@@ -36,9 +36,13 @@ EXTERNAL_DEPS_PATH = epath.Path(__file__).parent.parent / "external_deps"
 # The menagerie path is used to load robot assets.
 # Resource paths do not have glob implemented, so we use a bare epath.Path.
 MENAGERIE_PATH = EXTERNAL_DEPS_PATH / "mujoco_menagerie"
+# Commit SHA of the menagerie repo.
+MENAGERIE_COMMIT_SHA = "5e6af47a49bf8ae27581615b54045107b20ce584"
 
 
-def _clone_with_progress(repo_url: str, target_path: str) -> None:
+def _clone_with_progress(
+    repo_url: str, target_path: str, commit_sha: str
+) -> None:
   """Clone a git repo with progress bar."""
   process = subprocess.Popen(
       ["git", "clone", "--progress", repo_url, target_path],
@@ -75,6 +79,15 @@ def _clone_with_progress(repo_url: str, target_path: str) -> None:
   if process.returncode != 0:
     raise subprocess.CalledProcessError(process.returncode, ["git", "clone"])
 
+  # Checkout specific commit.
+  print(f"Checking out commit {commit_sha}")
+  subprocess.run(
+      ["git", "-C", target_path, "checkout", commit_sha],
+      check=True,
+      stdout=subprocess.PIPE,
+      stderr=subprocess.PIPE,
+  )
+
 
 def ensure_menagerie_exists() -> None:
   """Ensure mujoco_menagerie exists, downloading it if necessary."""
@@ -88,6 +101,7 @@ def ensure_menagerie_exists() -> None:
       _clone_with_progress(
           "https://github.com/deepmind/mujoco_menagerie.git",
           str(MENAGERIE_PATH),
+          MENAGERIE_COMMIT_SHA,
       )
       print("Successfully downloaded mujoco_menagerie")
     except subprocess.CalledProcessError as e:
